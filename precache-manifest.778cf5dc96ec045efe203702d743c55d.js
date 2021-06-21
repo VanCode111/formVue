2826e501705fb4a0e397520f27deebd1c5e5dfa4
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2021-06-22 01:25:17 +0300

    deploy

---
+++ precache-manifest.778cf5dc96ec045efe203702d743c55d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3046b49c38fa2cf38f8",
+    "url": "/formVue/css/app.b8cbda96.css"
+  },
+  {
+    "revision": "7da2026c8e75926cbe8e041b1306c75d",
+    "url": "/formVue/img/arrow-bottom.7da2026c.svg"
+  },
+  {
+    "revision": "757e4c2f2433db432f6e88f56587c952",
+    "url": "/formVue/img/check.757e4c2f.svg"
+  },
+  {
+    "revision": "e3380abb934aa9b037febf89edcc5b93",
+    "url": "/formVue/img/close.e3380abb.svg"
+  },
+  {
+    "revision": "6598b19ba97798077892ef79b82b1394",
+    "url": "/formVue/img/hand.6598b19b.png"
+  },
+  {
+    "revision": "b7117232e238fc19839d1c1a15cb631b",
+    "url": "/formVue/index.html"
+  },
+  {
+    "revision": "d3046b49c38fa2cf38f8",
+    "url": "/formVue/js/app.1f92edda.js"
+  },
+  {
+    "revision": "8ce136b48483773edf0f",
+    "url": "/formVue/js/chunk-vendors.a3a1a856.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/formVue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/formVue/robots.txt"
+  }
+]);
